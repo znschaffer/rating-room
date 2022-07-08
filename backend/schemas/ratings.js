8ab69e1f255264b6016b7d9171b5d517dbcba22c
Zane Schaffer <mail@zane.wiki>
2022-07-07 19:44:09 -0700

    Add Sanity

---
+++ backend/schemas/ratings.js
@@ -0,0 +1,27 @@
+export default {
+    title: "Ratings",
+    name: "ratings",
+    type: "document",
+    fields: [
+        {
+            title: "Name",
+            name: "name",
+            type: "string"
+        },
+        {
+            title: "Image",
+            name: "image",
+            type: "image"
+        },
+        {
+            title: "Zane's Rating",
+            name: "zrating",
+            type: "number"
+        },
+        {
+            title: "Xinrui's Rating",
+            name: "xrating",
+            type: "number"
+        }
+    ]
+}
