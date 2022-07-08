dc2099424901fa2f30038c10f9850b817515c16b
Xinrui Chen <xynree@gmail.com>
2022-07-08 07:31:24 -0700

    Created new schema type `emotion`, changed `ratings` type to an object and removed file

---
+++ backend/schemas/emotions.js
@@ -0,0 +1,23 @@
+export default {
+	title: 'Emotions',
+	name: 'emotion',
+	type: 'document',
+	description: 'Emotions that our main characters can display',
+	fields: [
+		{
+			title: 'Emotion name',
+			name: 'name',
+			type: 'string'
+		},
+    {
+      title: 'Zane image',
+      name: 'zane',
+      type: 'image'
+    },
+    {
+      title: 'Xinrui image',
+      name: 'xyn',
+      type: 'image'
+    }
+	],
+};
--- backend/schemas/products.js
+++ backend/schemas/products.js
@@ -1,3 +1,70 @@
+const ratings = {
+	title: 'Ratings',
+	name: 'rating',
+	type: 'array',
+    of: [
+        {
+            type: 'object', fields: [
+		{
+			title: 'Rater Name',
+			name: 'name',
+			type: 'string',
+            validation: Rule => Rule.required(),
+			options: {
+				list: [
+					{ title: 'Xinrui', value: 'xyn' },
+					{ title: 'Zane', value: 'zane' }
+				]
+			}
+		},
+		{
+			title: 'Rating Number',
+			name: 'rating',
+			type: 'number',
+			options: {
+				list: [
+					{ title: '⭐', value: 1 },
+					{ title: '⭐⭐', value: 2 },
+					{ title: '⭐⭐⭐', value: 3 },
+					{ title: '⭐⭐⭐⭐', value: 4 },
+					{ title: '⭐⭐⭐⭐⭐', value: 5 }
+				]
+			},
+            validation: Rule => Rule.required()
+		},
+        {
+            title: 'Comments',
+            name: 'comments',
+            type: 'text'
+        },
+         {
+            title: 'Rating Slug',
+            name: 'slug',
+            type: 'slug',
+            options: {
+                source: (doc, {parent}) => {
+                    console.log(doc, parent)
+                    return `${parent.name}_${parent.rating}_${doc.name}_${doc._id.slice(-6)}`}
+            }
+         },
+        {
+            title: 'Emotion',
+            name: 'emotion',
+            type: 'reference',
+            to: [{type: 'emotion'}]
+        }
+	],
+    preview: {
+        select: {
+            title: 'name',
+            subtitle: 'slug.current'
+        }
+    }
+}
+]
+};
+
+
 export default {
 	title: 'Products',
 	name: 'product',
@@ -46,14 +113,7 @@ export default {
 			name: 'description',
 			type: 'text'
 		},
-		{
-			title: 'Ratings',
-			name: 'ratings',
-			type: 'array',
-			of: [{ type: 'reference',
-             to: [{ type: 'rating' }],
-             disableNew: false 
-            }],
-		}
+    ratings,
+
 	]
 };
