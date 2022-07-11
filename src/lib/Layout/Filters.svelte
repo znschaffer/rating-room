e17f4fbb4078a80876e9c5986a947c4a37ad8771
Xinrui Chen <xynree@gmail.com>
2022-07-10 18:57:47 -0700

    Multiple filtering cleaned up for object syntax

--- src/lib/Layout/Filters.svelte
+++ src/lib/Layout/Filters.svelte
@@ -2,9 +2,9 @@
 	import { productsView, products, tags } from '$lib/stores';
 	import normalize from '$helpers/normalize';
 	import getAvgRating from '$helpers/getAvgRating';
-	export let selectedCat;
-	export let selectedRating;
+	export let filters;
 	export let reset;
+	let {selectedCat, selectedRating} = filters;
 
 	const stars = ['⭐ ⬆', '⭐⭐ ⬆', '⭐⭐⭐ ⬆', '⭐⭐⭐⭐ ⬆', '⭐⭐⭐⭐⭐ ⬆'];
 
@@ -27,21 +27,23 @@
 		}
 		switch (normalize(name)) {
 			case 'rating':
-				productsView.set(filterByRating($products, value));
-
+				if (selectedCat)
+					productsView.set(filterByCat(filterByRating($products, value), selectedCat));
+				else productsView.set(filterByRating($products, value));
 				break;
 			case 'category':
-				productsView.set(filterByCat($products, value));
+				if (selectedRating)
+					productsView.set(filterByRating(filterByCat($products, value), selectedRating));
+				else productsView.set(filterByCat($products, value));
 				break;
 		}
 	};
 
-	const { container, filterBar, filterTitle, filterSort, option } = {
+	const { container, filterBar, filterTitle, filterSort } = {
 		container: 'flex flex-col text-sm h-auto mb-4 mr-6',
 		filterBar: 'pl-8 p-2 pr-2  flex justify-between',
 		filterTitle: 'font-bold',
-		filterSort: 'p-1 pl-9  mr-12 hover:bg-blue-300 w-full',
-		option: 'focus:outline-none appearance-none'
+		filterSort: 'p-1 pl-9  mr-12 hover:bg-blue-300 w-full focus:outline-none'
 	};
 </script>
 
@@ -54,13 +56,14 @@
 	<select on:change={filter} class={filterSort} name="category" bind:value={selectedCat}>
 		<option select="selected" value={0}>Category</option>
 		{#each $tags as tag (tag.name)}
-			<option class={option} value={tag.name}>{tag.name}</option>
+			<option value={tag.name}>{tag.name}</option>
 		{/each}
 	</select>
+
 	<select on:change={filter} class={filterSort} name="rating" bind:value={selectedRating}>
 		<option select="selected" value={0}>Rating</option>
 		{#each stars as starValue, i (starValue)}
-			<option class={option} value={i + 1}>{starValue}</option>
+			<option value={i + 1}>{starValue}</option>
 		{/each}
 	</select>
 </div>
