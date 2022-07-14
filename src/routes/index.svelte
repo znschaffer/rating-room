cafb35108aac4b707ce47048631011de7d1ebb15
Xinrui Chen <xynree@gmail.com>
2022-07-13 22:28:44 -0700

    State handler for navigating with forward and back button added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,30 +4,37 @@
 	import { products, productsView, tags, currentProduct } from '$lib/stores';
 	import { browser } from '$app/env';
 	import normalize from '$helpers/normalize';
-	import {parseSlug} from '$helpers/parse';
+	import { parseSlug } from '$helpers/parse';
 
 	export let data;
 	products.set(data.products);
 	tags.set(data.tags);
 
 	productsView.set(data.products);
- 
- if (browser && window.location.search) {
-	const params = new URLSearchParams(window.location.search)
-	const paramProd = params.get('product');
+	const goToProduct = () => {
+		const params = new URLSearchParams(window.location.search);
+		const paramProd = params.get('product');
 
-	if (paramProd){ 
-		const foundProduct = $products.find(({name})=> normalize(name) === normalize(parseSlug(paramProd)))
-		if (foundProduct){
-			currentProduct.set(foundProduct)
+		if (paramProd) {
+			const foundProduct = $products.find(
+				({ name }) => normalize(name) === normalize(parseSlug(paramProd))
+			);
+			if (foundProduct) {
+				currentProduct.set(foundProduct);
+			}
 		} else {
 			const url = new URL(window.location.origin);
-
 			history.pushState({}, '', url);
+			currentProduct.set({});
+		}
+	};
+
+	if (browser) {
+		window.onpopstate = () => {
+			console.log('sttae poped')
+			goToProduct();
 		}
 	}
- }
-
 </script>
 
 <svelte:head>
@@ -36,8 +43,8 @@
 
 <div>
 	{#if Object.keys($currentProduct).length}
-	<Feature />
+		<Feature />
 	{:else}
-	<Grid products={$productsView} />
+		<Grid products={$productsView} />
 	{/if}
 </div>
