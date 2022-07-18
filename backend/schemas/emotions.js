1eb9921b0c764d3ed3deabad7640693a509a66e6
Zane Schaffer <mail@zane.wiki>
2022-07-18 15:49:35 -0700

    fixes #18

--- backend/schemas/emotions.js
+++ backend/schemas/emotions.js
@@ -1,3 +1,26 @@
+import client from 'part:@sanity/base/client';
+import groq from 'groq';
+
+const isUniqueEmotion = (emotion, context) => {
+	const { document } = context;
+
+	const id = document._id.replace(/^drafts\./, '');
+
+	const params = {
+		draft: `drafts.${id}`,
+		published: id,
+		emotion
+	};
+	/* groq */
+	const query = groq`!defined(*[
+    _type == 'emotion' &&
+    !(_id in [$draft, $published]) &&
+    name == $emotion
+  ][0]._id)`;
+
+	return client.fetch(query, params);
+};
+
 export default {
 	title: 'Emotions',
 	name: 'emotion',
@@ -7,7 +30,13 @@ export default {
 		{
 			title: 'Emotion name',
 			name: 'name',
-			type: 'string'
+			type: 'string',
+			validation: (Rule) =>
+				Rule.custom(async (value, context) => {
+					const isUnique = await isUniqueEmotion(value, context);
+					if (!isUnique) return 'Emotion is not unique';
+					return true;
+				})
 		},
 		{
 			title: 'Description',
