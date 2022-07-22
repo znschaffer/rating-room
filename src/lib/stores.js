2fab2aed8691ff71ed48b46c03ae5d84270e2556
Xinrui Chen <xynree@gmail.com>
2022-07-22 13:23:30 -0700

    feat: moved filters into stores, adjusted filter setting for defaults with structured clone

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,8 +1,10 @@
 import  { writable } from "svelte/store";
 
 export const products = writable([])
-export const currentProduct = writable({})
-export const productsView = writable([])
 export const emotions = writable([])
-
 export const tags = writable([])
+
+
+export const productsView = writable([])
+export const currentProduct = writable({})
+export const filters = writable({ selectedCat: 0, selectedRating: 0 })
