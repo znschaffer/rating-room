82bbf9a21cadf67c5382788d020ac8ea15bedcc3
Xinrui Chen <xynree@gmail.com>
2022-07-23 15:36:35 -0700

    update: param, toProduct helpers typed

--- src/helpers/toProduct.ts
+++ src/helpers/toProduct.ts
@@ -1,14 +1,12 @@
 import { parseSlug, normalize, setUrlParam, resetParams } from '.';
-import type { Product } from '../types';
-import type { UrlObject } from "node_modules/@types/node/url.d"
+import type { Product } from '$types';
+import type { Writable} from "svelte/store"
+
 
 /**
  * Updates Window URL and checks that it is equal to product name
- * @param {UrlObject} url a URL object
- * @param {Product} product product to check URL against
- * @returns {boolean} True if URL parameter is equal to product name
  */
-const didWinUrlUpdate = (url: UrlObject, product:Product): boolean => {
+const didWinUrlUpdate = (url: string, product:Product): boolean => {
 	window.history.pushState(window.history.state, '', url);
 	const params = new URLSearchParams(window.location.search).get('product');
 	return parseSlug(params) === normalize(product.name);
@@ -18,9 +16,7 @@ const didWinUrlUpdate = (url: UrlObject, product:Product): boolean => {
  * Takes a `product` and `currentProduct` store
  * Sets URL params to parsed product name and updates Browser URL
  * Sets `currentProduct` store to `product`.
- * @param {Product} product The product to switch feature to.
- * @param {any} currentProduct currentProduct store
  */
-export const toProduct = (product: Product, currentProduct: any) =>
-	didWinUrlUpdate(setUrlParam(new URL(window.location), product), product)?
+export const toProduct = (product: Product, currentProduct:Writable<Product>) =>
+	didWinUrlUpdate(setUrlParam((new URL(window.location.origin)), product), product)?
 	currentProduct.set(product): resetParams()
