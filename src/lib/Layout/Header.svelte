4d3bb8edd4b3da2e6ab53c94156d6247d04873c7
Xinrui Chen <xynree@gmail.com>
2022-10-09 17:53:45 -0700

    added mobile layout (#68)

--- src/lib/Layout/Header.svelte
+++ src/lib/Layout/Header.svelte
@@ -1,12 +1,19 @@
 <script>
 	export let reset;
+	import MediaQuery from 'svelte-media-query';
 </script>
 
-<a href="/" class="title" on:click={reset}> Rating Room </a>
+<MediaQuery query="(min-width: 640px)" let:matches>
+	<a href="/" class="title" on:click={reset} class:mobile={!matches}> Rating Room </a>
+</MediaQuery>
 
 <style lang="postcss">
 	.title {
 		@apply font-bold p-4 pr-0 snap-start sticky top-0 bg-white h-12 shadow-sm shadow-white;
+	}
+
+	.mobile {
+		@apply w-full text-center mx-auto;
 	}
 	a:hover {
 		background-image: url('dither.gif');
--- src/lib/Layout/Search.svelte
+++ src/lib/Layout/Search.svelte
@@ -1,15 +1,20 @@
 <script>
-	import { products, productsView } from '$lib/stores';
+	import { products, productsView, currentProduct } from '$lib/stores';
 	import { includesName } from '$helpers';
+	import MediaQuery from 'svelte-media-query';
 
 	const searchProducts = (e) => {
 		productsView.set($products.filter((product) => includesName(e.target.value, product)));
 	};
 </script>
 
-<div class="container">
-	<input on:input={searchProducts} class="input" placeholder="search" />
-</div>
+<MediaQuery query="(min-width: 640px)" let:matches>
+	{#if matches || !Object.keys($currentProduct).length}
+		<div class="container">
+			<input on:input={searchProducts} class="input" placeholder="search" />
+		</div>
+	{/if}
+</MediaQuery>
 
 <style lang="postcss">
 	.container {
@@ -17,6 +22,6 @@
 	}
 
 	.input {
-		@apply outline outline-1 w-28 focus:outline-blue-600 p-1 text-sm;
+		@apply outline outline-1 w-4/5 sm:w-28 focus:outline-blue-600 p-1 text-sm;
 	}
 </style>
